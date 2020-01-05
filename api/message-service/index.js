67b9f7bd3fcd1d000c101f48e193b40421e9bbf1
Marco Yeung <ballchy@gmail.com>
2020-01-05 10:01:41 -0500

    Project init code

---
+++ api/message-service/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const path = require('path')
+const config = require('config')
+
+const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+
+    // req.io = io;
+    next();
+});
+
+// db config
+const db = config.get('mongoURI')
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('Database connection established'))
+    .catch(err => {
+        console.log(Error, err.message);
+    });
+
+app.use('/api/messages', require('./routes/messages'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+// io.on('connection', function (socket) {
+//     console.log('a user conncected');
+// });
+
+// io.on('disconnect', function (socket) {
+//     console.log('a user has disconnected')
+// })
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
